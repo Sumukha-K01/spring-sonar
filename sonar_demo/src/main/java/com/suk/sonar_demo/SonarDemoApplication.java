c534fdf69c35398e7b2339aa3774c0e702e083fa
Sumukha <suk@extremenetworks.com>
2024-08-27 13:32:26 +0530

    commit-1

---
+++ sonar_demo/src/main/java/com/suk/sonar_demo/SonarDemoApplication.java
@@ -0,0 +1,13 @@
+package com.suk.sonar_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SonarDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SonarDemoApplication.class, args);
+	}
+
+}
---
+++ sonar_demo/src/test/java/com/suk/sonar_demo/SonarDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suk.sonar_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SonarDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
